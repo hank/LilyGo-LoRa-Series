278bdd69c04e213449e4335e5bd80d0809f61a47
hank <hank@jointsecurityarea.org>
2025-05-13 10:45:35 -0700

    Adding BME280_Minimal for Supreme debugging

---
+++ examples/Sensor/BME280_Minimal/LoRaBoards.cpp
@@ -0,0 +1,445 @@
+/**
+ * @file      boards.cpp
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2024  ShenZhen XinYuan Electronic Technology Co., Ltd
+ * @date      2024-04-24
+ * @last-update 2025-04-09
+ *
+ */
+
+#include "LoRaBoards.h"
+
+#include "soc/rtc.h"
+#ifdef ENABLE_BLE
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#endif
+
+#if defined(HAS_SDCARD) && !defined(SD_SHARE_SPI_BUS)
+SPIClass SDCardSPI(HSPI);
+#endif
+
+#if defined(ARDUINO_ARCH_ESP32)
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5,0,0)
+#include "hal/gpio_hal.h"
+#endif
+#include "driver/gpio.h"
+#endif //ARDUINO_ARCH_ESP32
+
+
+DISPLAY_MODEL *u8g2 = NULL;
+static DevInfo_t  devInfo;
+
+#ifdef HAS_GPS
+static bool find_gps = false;
+String gps_model = "None";
+#endif
+
+
+uint32_t deviceOnline = 0x00;
+
+
+#ifdef HAS_PMU
+XPowersLibInterface *PMU = NULL;
+bool     pmuInterrupt;
+
+static void setPmuFlag()
+{
+    pmuInterrupt = true;
+}
+#endif
+
+static void enable_slow_clock();
+
+
+bool beginPower()
+{
+#ifdef HAS_PMU
+    if (!PMU) {
+        PMU = new XPowersAXP2101(PMU_WIRE_PORT);
+        if (!PMU->init()) {
+            Serial.println("Warning: Failed to find AXP2101 power management");
+            delete PMU;
+            PMU = NULL;
+        } else {
+            Serial.println("AXP2101 PMU init succeeded, using AXP2101 PMU");
+        }
+    }
+
+    if (!PMU) {
+        PMU = new XPowersAXP192(PMU_WIRE_PORT);
+        if (!PMU->init()) {
+            Serial.println("Warning: Failed to find AXP192 power management");
+            delete PMU;
+            PMU = NULL;
+        } else {
+            Serial.println("AXP192 PMU init succeeded, using AXP192 PMU");
+        }
+    }
+
+    if (!PMU) {
+        return false;
+    }
+
+    deviceOnline |= POWERMANAGE_ONLINE;
+
+    PMU->setChargingLedMode(XPOWERS_CHG_LED_CTRL_CHG);
+
+    pinMode(PMU_IRQ, INPUT_PULLUP);
+    attachInterrupt(PMU_IRQ, setPmuFlag, FALLING);
+
+    if (PMU->getChipModel() == XPOWERS_AXP192) {
+
+        PMU->setProtectedChannel(XPOWERS_DCDC3);
+
+        // lora
+        PMU->setPowerChannelVoltage(XPOWERS_LDO2, 3300);
+        // gps
+        PMU->setPowerChannelVoltage(XPOWERS_LDO3, 3300);
+        // oled
+        PMU->setPowerChannelVoltage(XPOWERS_DCDC1, 3300);
+
+        PMU->enablePowerOutput(XPOWERS_LDO2);
+        PMU->enablePowerOutput(XPOWERS_LDO3);
+
+        //protected oled power source
+        PMU->setProtectedChannel(XPOWERS_DCDC1);
+        //protected esp32 power source
+        PMU->setProtectedChannel(XPOWERS_DCDC3);
+        // enable oled power
+        PMU->enablePowerOutput(XPOWERS_DCDC1);
+
+        //disable not use channel
+        PMU->disablePowerOutput(XPOWERS_DCDC2);
+
+        PMU->disableIRQ(XPOWERS_AXP192_ALL_IRQ);
+
+        PMU->enableIRQ(XPOWERS_AXP192_VBUS_REMOVE_IRQ |
+                       XPOWERS_AXP192_VBUS_INSERT_IRQ |
+                       XPOWERS_AXP192_BAT_CHG_DONE_IRQ |
+                       XPOWERS_AXP192_BAT_CHG_START_IRQ |
+                       XPOWERS_AXP192_BAT_REMOVE_IRQ |
+                       XPOWERS_AXP192_BAT_INSERT_IRQ |
+                       XPOWERS_AXP192_PKEY_SHORT_IRQ
+                      );
+
+    } else if (PMU->getChipModel() == XPOWERS_AXP2101) {
+
+#if defined(T_BEAM_S3_SUPREME)
+        //t-beam m.2 inface
+        //gps
+        PMU->setPowerChannelVoltage(XPOWERS_ALDO4, 3300);
+        PMU->enablePowerOutput(XPOWERS_ALDO4);
+
+        // lora
+        PMU->setPowerChannelVoltage(XPOWERS_ALDO3, 3300);
+        PMU->enablePowerOutput(XPOWERS_ALDO3);
+
+        // In order to avoid bus occupation, during initialization, the SD card and QMC sensor are powered off and restarted
+        if (ESP_SLEEP_WAKEUP_UNDEFINED == esp_sleep_get_wakeup_cause()) {
+            Serial.println("Power off and restart ALDO BLDO..");
+            PMU->disablePowerOutput(XPOWERS_ALDO1);
+            PMU->disablePowerOutput(XPOWERS_ALDO2);
+            PMU->disablePowerOutput(XPOWERS_BLDO1);
+            delay(250);
+        }
+
+        // Sensor
+        PMU->setPowerChannelVoltage(XPOWERS_ALDO1, 3300);
+        PMU->enablePowerOutput(XPOWERS_ALDO1);
+
+        PMU->setPowerChannelVoltage(XPOWERS_ALDO2, 3300);
+        PMU->enablePowerOutput(XPOWERS_ALDO2);
+
+        //Sdcard
+
+        PMU->setPowerChannelVoltage(XPOWERS_BLDO1, 3300);
+        PMU->enablePowerOutput(XPOWERS_BLDO1);
+
+        PMU->setPowerChannelVoltage(XPOWERS_BLDO2, 3300);
+        PMU->enablePowerOutput(XPOWERS_BLDO2);
+
+        //face m.2
+        PMU->setPowerChannelVoltage(XPOWERS_DCDC3, 3300);
+        PMU->enablePowerOutput(XPOWERS_DCDC3);
+
+        PMU->setPowerChannelVoltage(XPOWERS_DCDC4, XPOWERS_AXP2101_DCDC4_VOL2_MAX);
+        PMU->enablePowerOutput(XPOWERS_DCDC4);
+
+        PMU->setPowerChannelVoltage(XPOWERS_DCDC5, 3300);
+        PMU->enablePowerOutput(XPOWERS_DCDC5);
+
+
+        //not use channel
+        PMU->disablePowerOutput(XPOWERS_DCDC2);
+        // PMU->disablePowerOutput(XPOWERS_DCDC4);
+        // PMU->disablePowerOutput(XPOWERS_DCDC5);
+        PMU->disablePowerOutput(XPOWERS_DLDO1);
+        PMU->disablePowerOutput(XPOWERS_DLDO2);
+        PMU->disablePowerOutput(XPOWERS_VBACKUP);
+
+        // Set constant current charge current limit
+        PMU->setChargerConstantCurr(XPOWERS_AXP2101_CHG_CUR_500MA);
+
+        // Set charge cut-off voltage
+        PMU->setChargeTargetVoltage(XPOWERS_AXP2101_CHG_VOL_4V2);
+
+        // Disable all interrupts
+        PMU->disableIRQ(XPOWERS_AXP2101_ALL_IRQ);
+        // Clear all interrupt flags
+        PMU->clearIrqStatus();
+        // Enable the required interrupt function
+        PMU->enableIRQ(
+            XPOWERS_AXP2101_BAT_INSERT_IRQ    | XPOWERS_AXP2101_BAT_REMOVE_IRQ      |   //BATTERY
+            XPOWERS_AXP2101_VBUS_INSERT_IRQ   | XPOWERS_AXP2101_VBUS_REMOVE_IRQ     |   //VBUS
+            XPOWERS_AXP2101_PKEY_SHORT_IRQ    | XPOWERS_AXP2101_PKEY_LONG_IRQ       |   //POWER KEY
+            XPOWERS_AXP2101_BAT_CHG_DONE_IRQ  | XPOWERS_AXP2101_BAT_CHG_START_IRQ       //CHARGE
+            // XPOWERS_AXP2101_PKEY_NEGATIVE_IRQ | XPOWERS_AXP2101_PKEY_POSITIVE_IRQ   |   //POWER KEY
+        );
+
+    }
+
+    PMU->enableSystemVoltageMeasure();
+    PMU->enableVbusVoltageMeasure();
+    PMU->enableBattVoltageMeasure();
+
+    Serial.printf("=========================================\n");
+    if (PMU->isChannelAvailable(XPOWERS_DCDC1)) {
+        Serial.printf("DC1  : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_DCDC1)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_DCDC1));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_DCDC2)) {
+        Serial.printf("DC2  : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_DCDC2)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_DCDC2));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_DCDC3)) {
+        Serial.printf("DC3  : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_DCDC3)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_DCDC3));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_DCDC4)) {
+        Serial.printf("DC4  : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_DCDC4)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_DCDC4));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_DCDC5)) {
+        Serial.printf("DC5  : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_DCDC5)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_DCDC5));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_LDO2)) {
+        Serial.printf("LDO2 : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_LDO2)   ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_LDO2));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_LDO3)) {
+        Serial.printf("LDO3 : %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_LDO3)   ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_LDO3));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_ALDO1)) {
+        Serial.printf("ALDO1: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_ALDO1)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_ALDO1));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_ALDO2)) {
+        Serial.printf("ALDO2: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_ALDO2)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_ALDO2));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_ALDO3)) {
+        Serial.printf("ALDO3: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_ALDO3)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_ALDO3));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_ALDO4)) {
+        Serial.printf("ALDO4: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_ALDO4)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_ALDO4));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_BLDO1)) {
+        Serial.printf("BLDO1: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_BLDO1)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_BLDO1));
+    }
+    if (PMU->isChannelAvailable(XPOWERS_BLDO2)) {
+        Serial.printf("BLDO2: %s   Voltage: %04u mV \n",  PMU->isPowerChannelEnable(XPOWERS_BLDO2)  ? "+" : "-",  PMU->getPowerChannelVoltage(XPOWERS_BLDO2));
+    }
+    Serial.printf("=========================================\n");
+
+
+    // Set the time of pressing the button to turn off
+    PMU->setPowerKeyPressOffTime(XPOWERS_POWEROFF_4S);
+    uint8_t opt = PMU->getPowerKeyPressOffTime();
+    Serial.print("PowerKeyPressOffTime:");
+    switch (opt) {
+    case XPOWERS_POWEROFF_4S: Serial.println("4 Second");
+        break;
+    case XPOWERS_POWEROFF_6S: Serial.println("6 Second");
+        break;
+    case XPOWERS_POWEROFF_8S: Serial.println("8 Second");
+        break;
+    case XPOWERS_POWEROFF_10S: Serial.println("10 Second");
+        break;
+    default:
+        break;
+    }
+#endif
+    return true;
+}
+
+bool beginDisplay()
+{
+    Wire.beginTransmission(DISPLAY_ADDR);
+    if (Wire.endTransmission() == 0) {
+        Serial.printf("Find Display model at 0x%X address\n", DISPLAY_ADDR);
+        u8g2 = new DISPLAY_MODEL(U8G2_R0, U8X8_PIN_NONE);
+        u8g2->begin();
+        u8g2->clearBuffer();
+        u8g2->setFont(u8g2_font_inb19_mr);
+        u8g2->drawStr(0, 30, "LilyGo");
+        u8g2->drawHLine(2, 35, 47);
+        u8g2->drawHLine(3, 36, 47);
+        u8g2->drawVLine(45, 32, 12);
+        u8g2->drawVLine(46, 33, 12);
+        u8g2->setFont(u8g2_font_inb19_mf);
+        u8g2->drawStr(58, 60, "LoRa");
+        u8g2->sendBuffer();
+        u8g2->setFont(u8g2_font_fur11_tf);
+        delay(3000);
+        return true;
+    }
+
+    Serial.printf("Warning: Failed to find Display at 0x%0X address\n", DISPLAY_ADDR);
+    return false;
+}
+
+
+void setupBoards(bool disable_u8g2 )
+{
+    Serial.begin(115200);
+
+    Serial.println("setupBoards");
+
+#if defined(ARDUINO_ARCH_ESP32)
+    SPI.begin(RADIO_SCLK_PIN, RADIO_MISO_PIN, RADIO_MOSI_PIN);
+#elif defined(ARDUINO_ARCH_STM32)
+    SPI.setMISO(RADIO_MISO_PIN);
+    SPI.setMOSI(RADIO_MOSI_PIN);
+    SPI.setSCLK(RADIO_SCLK_PIN);
+    SPI.begin();
+#endif
+
+
+#if defined(HAS_SDCARD)
+#if defined(SD_SHARE_SPI_BUS)
+    // Share spi bus with lora , set lora cs,rst to high
+    pinMode(RADIO_CS_PIN, OUTPUT);
+    pinMode(RADIO_RST_PIN, OUTPUT);
+    digitalWrite(RADIO_CS_PIN, HIGH);
+    digitalWrite(RADIO_RST_PIN, HIGH);
+#else
+    // SDCardSPI.begin(SDCARD_SCLK, SDCARD_MISO, SDCARD_MOSI);
+#endif /*SD_SHARE_SPI_BUS*/
+#endif /*HAS_SDCARD*/
+
+#ifdef I2C_SDA
+    Wire.begin(I2C_SDA, I2C_SCL);
+    Serial.println("Scan Wire...");
+    scanDevices(&Wire);
+#endif
+
+#ifdef I2C1_SDA
+    Wire1.begin(I2C1_SDA, I2C1_SCL);
+    Serial.println("Scan Wire1...");
+    scanDevices(&Wire1);
+#endif
+    beginPower();
+    //beginSDCard();
+
+    if (!disable_u8g2) {
+        beginDisplay();
+    }
+
+    // scanWiFi();
+
+    // beginWiFi();
+    enable_slow_clock();
+    Serial.println("init done . ");
+}
+
+void scanDevices(TwoWire *w)
+{
+    uint8_t err, addr;
+    int nDevices = 0;
+    uint32_t start = 0;
+
+    Serial.println("I2C Devices scanning");
+    for (addr = 1; addr < 127; addr++) {
+        start = millis();
+        w->beginTransmission(addr); delay(2);
+        err = w->endTransmission();
+        if (err == 0) {
+            nDevices++;
+            switch (addr) {
+            case 0x77:
+            case 0x76:
+                Serial.println("\tFind BMX280 Sensor!");
+                deviceOnline |= BME280_ONLINE;
+                break;
+            case 0x34:
+                Serial.println("\tFind AXP192/AXP2101 PMU!");
+                deviceOnline |= POWERMANAGE_ONLINE;
+                break;
+            case 0x3C:
+                Serial.println("\tFind SSD1306/SH1106 dispaly!");
+                deviceOnline |= DISPLAY_ONLINE;
+                break;
+            case 0x51:
+                Serial.println("\tFind PCF8563 RTC!");
+                deviceOnline |= PCF8563_ONLINE;
+                break;
+            case 0x1C:
+                Serial.println("\tFind QMC6310 MAG Sensor!");
+                deviceOnline |= QMC6310_ONLINE;
+                break;
+            default:
+                Serial.print("\tI2C device found at address 0x");
+                if (addr < 16) {
+                    Serial.print("0");
+                }
+                Serial.print(addr, HEX);
+                Serial.println(" !");
+                break;
+            }
+
+        } else if (err == 4) {
+            Serial.print("Unknow error at address 0x");
+            if (addr < 16) {
+                Serial.print("0");
+            }
+            Serial.println(addr, HEX);
+        }
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+
+    Serial.println("Scan devices done.");
+    Serial.println("\n");
+}
+#define CALIBRATE_ONE(cali_clk) calibrate_one(cali_clk, #cali_clk)
+
+static uint32_t calibrate_one(rtc_cal_sel_t cal_clk, const char *name)
+{
+    const uint32_t cal_count = 1000;
+    const float factor = (1 << 19) * 1000.0f;
+    uint32_t cali_val;
+    for (int i = 0; i < 5; ++i) {
+        cali_val = rtc_clk_cal(cal_clk, cal_count);
+    }
+    return cali_val;
+}
+
+static void enable_slow_clock()
+{
+    rtc_clk_32k_enable(true);
+    CALIBRATE_ONE(RTC_CAL_RTC_MUX);
+    uint32_t cal_32k = CALIBRATE_ONE(RTC_CAL_32K_XTAL);
+    if (cal_32k == 0) {
+        Serial.printf("32K XTAL OSC has not started up");
+    } else {
+        rtc_clk_slow_freq_set(RTC_SLOW_FREQ_32K_XTAL);
+        Serial.println("Switching RTC Source to 32.768Khz succeeded, using 32K XTAL");
+        CALIBRATE_ONE(RTC_CAL_RTC_MUX);
+        CALIBRATE_ONE(RTC_CAL_32K_XTAL);
+    }
+    CALIBRATE_ONE(RTC_CAL_RTC_MUX);
+    CALIBRATE_ONE(RTC_CAL_32K_XTAL);
+    if (rtc_clk_slow_freq_get() != RTC_SLOW_FREQ_32K_XTAL) {
+        Serial.println("Warning: Failed to set rtc clk to 32.768Khz !!! "); return;
+    }
+    deviceOnline |= OSC32768_ONLINE;
+}
+
+#endif /*ARDUINO_ARCH_ESP32*/
+
